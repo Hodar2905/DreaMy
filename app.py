a61e0f6a70ede19e78c0cf23d8961ab087a51a39
Hodar2905 <khadijahodar98@gmail.com>
2026-04-15 22:55:44 +0100

    fix viewer pdf

--- app.py
+++ app.py
@@ -63,8 +63,8 @@ def show_pdf(file):
     file.seek(0)
     base64_pdf = base64.b64encode(file.read()).decode("utf-8")
     pdf_display = f"""
-    <iframe src="data:application/pdf;base64,{base64_pdf}" 
-    width="100%" height="600px"></iframe>
+    <embed src="data:application/pdf;base64,{base64_pdf}" 
+    width="100%" height="600px" type="application/pdf">
     """
     st.markdown(pdf_display, unsafe_allow_html=True)
 
